9d34ab5ed4d6f14bc20b7544850bf0c2c543d2f5
Sahil Suthar <sahil26dd@gmail.com>
2025-08-19 20:24:00 +0530

    feat: Desktop version base setup

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+function greet() {
+  console.log(
+    'Hello Good Day Mate!!! \nPage Loaded sucessfully with parcel!!! \nI see it now ^_^'
+  );
+}
+
+document.addEventListener('DOMContentLoaded', greet);

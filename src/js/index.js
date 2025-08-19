367c11c4f5cc7dea8a0a9a218f0830c179d57be0
Sahil Suthar <sahil26dd@gmail.com>
2025-08-19 23:56:05 +0530

    IMPORTANT-NOTE: removed commented code - later review the changes

--- src/js/index.js
+++ src/js/index.js
@@ -8,51 +8,6 @@ function greet() {
 }
 document.addEventListener('DOMContentLoaded', greet);
 
-/*
-// 1) Dropdown - Logic
-function setupDropdown(dropdownId, hiddenInputId, onSelect = null) {
-  const dropdown = document.getElementById(dropdownId);
-  const selected = dropdown.querySelector('.dropdown-selected');
-  const options = dropdown.querySelectorAll('.dropdown-options li');
-  const hiddenInput = document.getElementById(hiddenInputId);
-
-  selected.addEventListener('click', () => {
-    dropdown.classList.toggle('active');
-  });
-
-  options.forEach(option => {
-    option.addEventListener('click', () => {
-      selected.textContent = option.textContent;
-      hiddenInput.value = option.dataset.value;
-
-      options.forEach(opt => opt.classList.remove('selected'));
-      option.classList.add('selected');
-
-      dropdown.classList.remove('active');
-
-      if (onSelect) onSelect(option.dataset.value);
-    });
-  });
-
-  document.addEventListener('click', e => {
-    if (!dropdown.contains(e.target)) {
-      dropdown.classList.remove('active');
-    }
-  });
-}
-
-setupDropdown('queryTypeDropdown', 'queryTypeInput', value => {
-  const projectDropdown = document.getElementById('projectDropdown');
-  if (value === 'projects') {
-    projectDropdown.classList.remove('hidden');
-  } else {
-    projectDropdown.classList.add('hidden');
-  }
-});
-
-setupDropdown('projectDropdown', 'projectInput');
-*/
-
 // 1) Dropdown - Logic
 initDropdowns();
 // Initial render
